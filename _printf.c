7eb3766a99631de637844df56d13fedbb1601907
Gatwan <abigailgathigia@gmail.com>
2022-10-19 15:38:48 +0000

    updates to functions.c

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 
 	va_start(valist, format);
 
-
+	
 
 	va_end(valist);
 
--- functions.c
+++ functions.c
@@ -2,4 +2,32 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+*printchar - function that prints a character
+*@valist: loop through the list of arguments
+*Return: char length as int
+*/
+int printchar(va_list valist)
+{
+	int printc = 0;
 
+	printc = va_arg(valist, int);
+	_putchar(printc);
+	return  (1);
+}
+
+/**
+*printstr - function that prints a string
+*@valist: loop through the list of arguments
+*Return: length of string as int
+*/
+int printstr(va_list valist)
+{
+	char *c = va_arg(valist, char *);
+
+	if (c == NULL)
+	{
+		_puts("(null)");
+		return (6);
+	}
+}
