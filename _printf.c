bd9fa5aee56f38de20dedf595eae5dbb68d41917
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-19 15:28:01 +0000

    updated _printf function

--- _printf.c
+++ _printf.c
@@ -12,14 +12,43 @@
 int _printf(const char *format, ...)
 {
 	va_list valist;
+	int b = 0, c = 0, d = 0, e = 0, cuenf = 0, len = 0;
+
 	specifier my_list[] = {
 		{"c", printchar}, {"s", printstr}
 	};
 
 	va_start(valist, format);
-
-
-
+	if (format == NULL)
+	{
+	return (-1);
+	}
+	while (format != NULL && format[b] != 0)
+	{
+		c = 0;
+		if (format[b] == '%')
+		{
+			for (d = 0; d < 8; d++)
+			{
+				if (format[b + 1] == 0)
+					{
+					return (-1);
+					}
+				if (format[b + 1] == *(my_list[d].fs))
+				{
+					cuenf = cuenf + my_list[d].func(valist);
+					c = 2;
+					e = e + 2;
+					b = b + 1;
+					break;
+				}
+			}
+		}
+		if (c == 0)
+			_putchar(format[b]);
+		b = b + 1;
+	}
+	len = b + cuenf - e;
 	va_end(valist);
-
+	return (len);
 }
