22861889102619970c006a9ae1513aacbfbf536a
Gatwan <abigailgathigia@gmail.com>
2022-10-19 09:22:46 +0000

    First commit printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *_printf - function that produces output according to a format
 *
@@ -8,5 +9,30 @@
 */
 int _printf(const char *format, ...)
 {
+	unsigned int i = 0, j, count;
+	char * str;
+	const char t_arg[] = "cs";
 
+	va_list ap;
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && count)
+			{
+				printf(", ");
+				break;
+			} 
+		j++;
+		}
+		switch(format[i])
+		{
+			case 'c';
+			printf
+		}
+	}
 }
